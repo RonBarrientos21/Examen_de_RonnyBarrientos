f4f015867eab7e62576460072081674bb56bc749
RonBarrientos21 <rbarrientosa1@miumg.edu.gt>
2022-03-06 18:27:07 -0600

    se agrega la funcion para determinar cantidad de numero pares e impares (Primer Examen Parcial/Programacion1)

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@ int factorial(int n)
 }
 
 int main(void) {
+
+
     int num, fact;
     printf("Ingrese un entero igual o mayor que cero: ");
     scanf("%d", &num);
@@ -32,5 +34,28 @@ int main(void) {
     for (int i = 1; i <= 10; i++) {
         cout << N << " x " << i << " = " << N * i << endl;
     }
+    int N2;
+    int numero;
+    int contPares = 0;
+    int contImpares = 0;
+
+
+    cout<<"Ingresar la cantidad de numero para analizar:";
+    cin>>N2;
+    for(int i=1; i<= N2;i++) {
+        cout << "ingrese un numero:";
+        cin >> numero;
+        if (numero % 2 == 0) { //es par
+            contPares++;
+        } else {
+            contImpares++;
+
+        }
+
+    }
+    cout << "La cant de pares es:" << contPares << endl;
+    cout << "La cant de impares es:" << contImpares << endl;
+
+
     return (0);
 }
